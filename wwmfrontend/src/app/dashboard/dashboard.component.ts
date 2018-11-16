22065cd02913bcaa59a74f664a5dd90bd9a8c06d
afmontero <33687152+afmontero@users.noreply.github.com>
2018-11-16 21:27:41 +0100

    Añadido nombre a bienviendo

--- wwmfrontend/src/app/dashboard/dashboard.component.ts
+++ wwmfrontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { PollService } from './../services/poll.service';
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   arrayCard: Array<Card>;
+  user: any;
 
-  constructor(private pollService: PollService) { }
+  constructor(private pollService: PollService, private loginService: LoginService) { }
 
   ngOnInit() {
     this.pollService.getAll().subscribe(res => {
       console.log(res);
       this.arrayCard = res as Card[];
     }, err => {
+      console.log(err);
+    });
+    
+    this.loginService.getUser().subscribe(res => {
+      console.log(res); 
+      this.user=res;
+    }, err =>{
       console.log(err);
     });
 
